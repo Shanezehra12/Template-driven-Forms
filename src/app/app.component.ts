3583fb3e4a643fd2f864de41279d13ea8be18308
unknown <shanelakhani52@gmail.com>
2023-10-25 03:21:06 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf-forms';
+  topics = ['Angular' , 'React' , 'Vue']
+
+  userModel = new User('shane' , 'shane@test.com', 123456789, 'default', 'Morning' , true);
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+   validateTopic(value: any) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        response => console.log('Success!', response),
+        error => this.errorMsg = error.statusText
+      )
+  } 
 }
